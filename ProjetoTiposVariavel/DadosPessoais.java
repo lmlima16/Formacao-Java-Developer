170cda21bc82eabe40fa0b70203eb297677e5dc9
Livson Lima <livsonlima@gmail.com>
2025-12-19 16:28:34 -0300

    inclusão da classe Dados Pessoais

---
+++ ProjetoTiposVariavel/DadosPessoais.java
@@ -0,0 +1,17 @@
+public class DadosPessoais {
+  public static void main(String[] args) {
+    String nome = "Davi Santos";
+    String endereco = "Rua Acre, 100 - Teresópolis - RJ";
+    int cep = 66810-192;
+    String telefone = "(21) 9 6558-8922";
+    String email = "livsonlima@yahoo.com.br";
+
+    System.out.println("\nNome: " + nome);
+    System.out.println("Endereço: " + endereco);
+    System.out.println("CEP: " + cep);
+    System.out.println("Telefone: " + telefone);
+    System.out.println("\nEmail: " + email);
+
+  }
+
+}
---
+++ ProjetoTiposVariavel/SalarioMensal.java
@@ -0,0 +1,11 @@
+public class SalarioMensal {
+	public static void main(String[] args) {
+	
+  Float salarioMensal = 2300.61F ;    // Salario Base
+	salarioMensal = 2300.61F + 1500.00F; // Hora Extras
+	
+	System.out.println("Salario Mensal: R$ " + salarioMensal);
+	
+	}
+
+}
