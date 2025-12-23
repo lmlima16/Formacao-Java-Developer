adebe14a717ab464fef802151f694fe4e6fafca1
Livson Lima <livsonlima@gmail.com>
2025-12-22 21:02:58 -0300

    Add class Operadores

---
+++ ProjetoTiposVariavel/CalculadoraBasica.java
@@ -0,0 +1,16 @@
+public class CalculadoraBasica {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		
+		int soma = num1 + num2;
+    int subtracao = num1 - num2;
+    int multiplicacao = num1 * num2;
+    int divisao = num1 / num2;
+    	
+	System.out.println("\n == Resultado: " + soma);
+	System.out.println("\n == Resultado: " + subtracao);
+	System.out.println("\n == Resultado: " + multiplicacao);
+	System.out.println("\n == Resultado: " + divisao);
+
+}
