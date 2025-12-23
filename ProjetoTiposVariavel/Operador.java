8a68bca5caad9c5c4c5bbc3349c0afb0e4216b56
Livson Lima <livsonlima@gmail.com>
2025-12-22 22:40:48 -0300

    add operador unario positivo e nagativo - jogo de sinal

---
+++ ProjetoTiposVariavel/Operador.java
@@ -0,0 +1,11 @@
+public class Operador {
+  public static void main(String[] args) {
+  int numero = 5;
+  numero = +numero;
+
+  System.out.print("\n- Primeiro: " + -numero);
+  System.out.print("\n - Segundo:" + numero);
+ 
+  }
+  
+}
