1a3bfc0718a150c58bb09c44c896991e7af28420
Livson Lima <livsonlima@gmail.com>
2026-01-03 22:56:37 -0300

    Adicionando classe ClasseOpRelacionais

--- ProjetoTiposVariavel/ClasseOperadores.java
+++ ProjetoTiposVariavel/ClasseOperadores.java
@@ -1,19 +1,19 @@
-public class Operadores {
+public class ClasseOperadores {
     public static void main(String[] args) {
         int a, b;
 
-        a = 5;
+        a = 4;
         b = 6;
 
-        String resultado = "";
+        // String resultado = a == b ? "verdadeiro" : "falso";
+        int resultado = a == b ? 1 : 0; // usando int para representar verdadeiro/falso "com operadores ternario"
 
-        if (a == b) {
-            resultado = "verdadeiro";
-        } else {
-            resultado = "falso";
-        }
+        // if (a == b) 
+            // resultado = "verdadeiro";
+        //   } else {
+       //     resultado = "falso";
 
-        System.out.println("\nresultado = " + resultado);
+        System.out.println("\nO resultado da comparação é: " + resultado);
 
     }
 
