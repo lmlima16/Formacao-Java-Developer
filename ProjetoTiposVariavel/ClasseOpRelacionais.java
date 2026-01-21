5e1a2527ebf241184a6e93970e88cc204a996f38
Livson Lima <livsonlima@gmail.com>
2026-01-21 18:07:11 -0300

    21.01.2026 - Salvando

---
+++ ProjetoTiposVariavel/ClasseOpRelacionais.java
@@ -0,0 +1,19 @@
+public class ClasseOpRelacionais {
+  public static void main(String[] args) {
+    int numero1 = 1;
+    int numero2 = 2;
+    boolean simNao = numero1==numero2;
+
+    System.out.println("\nO numero1 é igual ao numero2 ? " + simNao);
+
+    simNao = numero1 != numero2;
+
+    System.out.println("\nO numeroUm é diferente do numeroDois ? " + simNao);
+
+   simNao = numero1 < numero2;
+
+    System.out.println("\nO numeroUm é menor que o numeroDois ? " + simNao);
+
+  }
+
+}
